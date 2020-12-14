ba1b864fb83c88c035d92f1cced5469bca20bd45
Mikhail Lapin <mlapin@rambler.ru>
2020-12-14 16:44:36 +0300

    44

--- l3032clock/app/src/main/java/com/example/freon/l3032clock/MainActivity.kt
+++ l3032clock/app/src/main/java/com/example/freon/l3032clock/MainActivity.kt
@@ -1,17 +1,11 @@
-package com.example.l2142webview
+package com.example.freon.l3032clock
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.webkit.WebView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        var webView = findViewById<WebView>(R.id.webView)
-        webView.settings.javaScriptEnabled = true
-
-        webView.loadUrl("file:///android_asset/item_3.html")
     }
 }
