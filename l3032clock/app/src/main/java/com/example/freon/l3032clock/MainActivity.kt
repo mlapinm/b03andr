1d78ba03fb9b48efc9af2ea38a1fe4cc4f352ef7
Mikhail Lapin <mlapin@rambler.ru>
2020-12-22 16:11:39 +0300

    5

--- l3032clock/app/src/main/java/com/example/freon/l3032clock/MainActivity.kt
+++ l3032clock/app/src/main/java/com/example/freon/l3032clock/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.freon.l3032clock
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PowerManager
+import android.view.WindowManager
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+
     }
+
 }
