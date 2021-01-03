77fcda5f5d09f0e92df3bdae70bde424147a2372
mlapinm <mlapin@rambler.ru>
2021-01-03 12:24:41 +0300

    b3062fb

---
+++ b3062fb/app/src/main/java/com/example/b3062fb/MainActivity.java
@@ -0,0 +1,105 @@
+package com.example.b3062fb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText editTextTitle;
+    private EditText editTextDescription;
+    private EditText editTextPriority;
+    private EditText editTextTags;
+    private TextView textViewData;
+    private View view;
+
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private CollectionReference notebookRef = db.collection("Notebook");
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        view = findViewById(R.layout.activity_main);
+
+        editTextTitle = findViewById(R.id.edit_text_title);
+        editTextDescription = findViewById(R.id.edit_text_description);
+        editTextPriority = findViewById(R.id.edit_text_priority);
+        editTextTags = findViewById(R.id.edit_text_tags);
+        textViewData = findViewById(R.id.text_view_data);
+
+        updateNestedValue();
+        this.loadNotes(view);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    public void addNote(View view) {
+        String title = editTextTitle.getText().toString();
+        String description = editTextDescription.getText().toString();
+        if (editTextPriority.length() == 0) {
+            editTextPriority.setText("0");
+        }
+
+        int priority = Integer.parseInt(editTextPriority.getText().toString());
+
+        String tagInput = editTextTags.getText().toString();
+        String[] tagArray = tagInput.split("\\s*,\\s*");
+        Map<String, Boolean> tags = new HashMap<>();
+
+        for(String tag: tagArray){
+            tags.put(tag, true);
+        }
+        Note note = new Note(title, description, priority, tags);
+        notebookRef.add(note);
+    }
+
+    public void loadNotes(View view) {
+        notebookRef
+                .whereEqualTo("tags.tag1", true)
+                .get()
+                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                    @Override
+                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                        String data = "";
+
+                        for(QueryDocumentSnapshot documentSnapshot: queryDocumentSnapshots){
+                            Note note = documentSnapshot.toObject(Note.class);
+                            note.setDocumentId(documentSnapshot.getId());
+
+                            String documentId = note.getDocumentId();
+                            data += "Document ID: " + documentId;
+
+                            for(String tag: note.getTags().keySet()){
+                                data += "\n - " + tag;
+                            }
+                            data += "\n";
+                        }
+                        textViewData.setText(data);
+                    }
+                });
+    }
+
+    private void updateNestedValue() {
+        notebookRef.document("XjDFeFxDrHhSKbyaigdP")
+//                .update("tags.tag1", false);
+//                .update("tags.tag1", FieldValue.delete());
+                .update("tags.tag1.nested1.nested2", true);
+    }
+}
---
+++ b3062fb/app/src/main/java/com/example/b3062fb/Note.java
@@ -0,0 +1,48 @@
+package com.example.b3062fb;
+
+import com.google.firebase.firestore.Exclude;
+import java.util.List;
+import java.util.Map;
+
+public class Note {
+    private String documentId;
+    private String title;
+    private  String description;
+    private int priority;
+    Map<String, Boolean> tags;
+
+    public Note() {
+    }
+
+    public Note(String title, String description, int priority, Map<String, Boolean> tags){
+        this.title = title;
+        this.description = description;
+        this.priority = priority;
+        this.tags = tags;
+    }
+
+    @Exclude
+    public String getDocumentId() {
+        return documentId;
+    }
+
+    public void setDocumentId(String documentId) {
+        this.documentId = documentId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public Map<String, Boolean> getTags() {
+        return tags;
+    }
+}
